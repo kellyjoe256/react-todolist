75dd363a97e59eccb4476a68343382d184812c58
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2020-01-16 09:22:35 +0300

    Updates

---
+++ precache-manifest.ffea8f04d0c21eea7d752f283c8f57cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d5706db69584ebb630ca51669258c6",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "509da7af9aabd4adc57c",
+    "url": "/react-todolist/static/css/2.1d10ae30.chunk.css"
+  },
+  {
+    "revision": "a4043918af2a6b47280e",
+    "url": "/react-todolist/static/css/main.cc4517c6.chunk.css"
+  },
+  {
+    "revision": "509da7af9aabd4adc57c",
+    "url": "/react-todolist/static/js/2.7aab48f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todolist/static/js/2.7aab48f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4043918af2a6b47280e",
+    "url": "/react-todolist/static/js/main.e4513712.chunk.js"
+  },
+  {
+    "revision": "e680a96c58007334716e",
+    "url": "/react-todolist/static/js/runtime-main.6eb98b22.js"
+  }
+]);
